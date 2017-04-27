d9d8dfe105917c8baf7b1fcfd352467d64de1f8d
phickey95 <mangopangotango@hotmail.com>
2017-04-26 23:55:11 -0400

    added laplace smoothing to filtered features/emoticons

--- Code/analytics.py
+++ Code/analytics.py
@@ -6,6 +6,7 @@ import ast
 #s = pd.Series({'a':1, 'b':2, 'c':3})
 #print s.values # a numpy array
 
+#len() == 34
 emoticons_list = [';D', ':D', ';)', ':)', ';]' ':]', ':|', ':/', ':(', ':[', ':p', ':P', ':o', ':O', '=)', '=]', '=(', '=[',
                     ':-]', ':-[',':-D',':-0', ':-o', ':-O', ':-P', ':-p', ':-|', ':-/', ';-]', ';-P', ';-p', ":'(", ":'[", "='(", "='[" ]
 
@@ -19,17 +20,25 @@ with open('Data/pickles/pos_bof.pkl', 'rb') as f:
     #dictionary = ast.literal_eval(pickle.load(f))
     for key,val in pickle.load(f).iteritems():
         if key in emoticons_list:
-            pos_emot[key] = val
+            #laplace smoothing
+            pos_emot[key] = val+1
         if val > 1 and key not in emoticons_list:
-            pos_filtered_features[key] = val
+            pos_filtered_features[key] = val+1
             pos_above_one_ct += 1
         if key in emoticons_list:
             #always add emoticon counts, even if zero
-            pos_filtered_features[key] = val
+            pos_filtered_features[key] = val+1
             if val > 1:
                 pos_above_one_ct += 1
+    #laplace smoothing for icons not found in class
+    for icon in emoticons_list:
+        if icon not in pos_filtered_features:
+            pos_filtered_features[icon] = 1
+        if icon not in pos_emot:
+            pos_emot[icon] = 1
     print '   above_one_ct: ' + str(pos_above_one_ct)
     print pos_emot
+    #print '   pos_emot length: ' +str(len(pos_emot))
 
 with open('Data/pickles/pos_bof.pkl', 'rb') as f:
     print '   unfiltered #features: ' + str(len(pickle.load(f)) )
@@ -47,17 +56,25 @@ with open('Data/pickles/neg_bof.pkl', 'rb') as f:
     #dictionary = ast.literal_eval(pickle.load(f))
     for key,val in pickle.load(f).iteritems():
         if key in emoticons_list:
-            neg_emot[key] = val
+            #laplace smoothing
+            neg_emot[key] = val+1
         if val > 1 and key not in emoticons_list:
-            neg_filtered_features[key] = val
+            neg_filtered_features[key] = val+1
             neg_above_one_ct += 1
         if key in emoticons_list:
             #always add emoticon counts, even if zero
-            neg_filtered_features[key] = val
+            neg_filtered_features[key] = val+1
             if val > 1:
                 neg_above_one_ct += 1
+    #laplace smoothing for icons not found in class
+    for icon in emoticons_list:
+        if icon not in neg_filtered_features:
+            neg_filtered_features[icon] = 1
+        if icon not in neg_emot:
+            neg_emot[icon] = 1
     print '   above_one_ct: ' + str(neg_above_one_ct)
     print neg_emot
+    #print '   neg_emot length: ' +str(len(neg_emot))
 
 with open('Data/pickles/neg_bof.pkl', 'rb') as f:
     print '   unfiltered #features: ' + str(len(pickle.load(f)) )
@@ -73,8 +90,8 @@ for n_key, n_val in neg_filtered_features.iteritems():
             intersection[n_key] = (n_val, p_val)
 print "   intersection length: " + str(len(intersection))
 #following comes from printed output & is hardcoded since im lazy
-print "   unique #pos. filtered~features: " + str(30132-17027)
-print "   unique #neg. filtered~features: " + str(27601-17027)
+print "   unique #pos. filtered~features: " + str(30144-17040)
+print "   unique #neg. filtered~features: " + str(27612-17040)
 print "+=+=+=+=+=+=+=+=+="
 print "   writing filtered data to pickle files..."
 #write BOFs to files
--- Code/temp.py
+++ Code/temp.py
@@ -2,6 +2,9 @@ from textblob import TextBlob
 import pickle
 import numpy as np
 
+emoticons_list = [';D', ':D', ';)', ':)', ';]' ':]', ':|', ':/', ':(', ':[', ':p', ':P', ':o', ':O', '=)', '=]', '=(', '=[',
+                    ':-]', ':-[',':-D',':-0', ':-o', ':-O', ':-P', ':-p', ':-|', ':-/', ';-]', ';-P', ';-p', ":'(", ":'[", "='(", "='[" ]
+
 text = '''
     thats a dog
 '''
@@ -13,10 +16,13 @@ print TextBlob(text).noun_phrases
 print "==========="
 for text in TextBlob(text).tags:
     print text[1]
-
+'''
 dictionary = {'hello': 100}
 with open('Data/test.pkl', 'wb') as f:
     pickle.dump(dictionary, f, pickle.HIGHEST_PROTOCOL)
 
 with open('Data/test.pkl', 'rb') as f:
     print pickle.load(f)
+'''
+
+print len(emoticons_list)
